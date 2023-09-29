1d9938e9c98acdb9a12548effbcbde2c40343656
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-09-29 12:32:38 +0200

    Update app_window.py

--- src/app_window.py
+++ src/app_window.py
@@ -3,10 +3,12 @@ import sys
 import os
 import locale
 import json
+import subprocess
 import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 from gi.repository import Gtk, Adw, Gio, GLib
+from pathlib import Path
 
 p_lang = locale.getlocale()[0]
 r_lang = p_lang[:-3]
@@ -17,6 +19,64 @@ except:
     locale = open("/app/translations/en.json")
     
 _ = json.load(locale)
+DATA = f"{Path.home()}/.var/app/io.github.vikdevelop.BinaryTranslator/data"
+
+class Dialog_set(Adw.MessageDialog):
+    def __init__(self, *args, **kwargs):
+        super().__init__(transient_for=app.get_active_window(), **kwargs)
+        self.set_heading("Saved binary texts")
+        self.set_body_use_markup(True)
+        
+        self.settings = Gio.Settings.new_with_path("io.github.vikdevelop.BinaryTranslator", "/io/github/vikdevelop/BinaryTranslator/")
+        
+        # Box for appending widgets
+        self.setdBox = Gtk.ListBox.new()
+        self.setdBox.set_selection_mode(mode=Gtk.SelectionMode.NONE)
+        self.setdBox.get_style_context().add_class(class_name='boxed-list')
+        self.set_extra_child(self.setdBox)
+        
+        if os.path.exists(f"{DATA}/binaries.txt"):
+            self.binaries_in = subprocess.getoutput(f"cat {DATA}/binaries.txt")
+            self.binaries_out = self.binaries_in.split()
+            if self.binaries_out == []:
+                self.set_body("You have not any binary text")
+                self.add_response('cancel', "Cancel")
+            else:
+                self.add_response('remove', "Remove")
+                self.show_text()
+        else:
+            self.set_body("You have not any binary text")
+            self.add_response('cancel', "Cancel")
+            
+        self.set_response_appearance('remove', Adw.ResponseAppearance.DESTRUCTIVE)
+        self.connect('response', self.dialog_response)
+        
+        self.show()
+        
+    def show_text(self):
+        actions = Gtk.StringList.new(strings=self.binaries_out)
+        
+        self.import_row = Adw.ComboRow.new()
+        self.import_row.set_use_markup(True)
+        self.import_row.set_use_underline(True)
+        self.import_row.set_title("")
+        self.import_row.set_title_lines(2)
+        self.import_row.set_subtitle_lines(4)
+        self.import_row.set_model(model=actions)
+        self.setdBox.append(child=self.import_row)
+        self.add_response('ok', "Use")
+        
+    def dialog_response(self, dialog, response):
+        sel_item = self.import_row.get_selected_item()
+        if response == 'ok':
+            self.settings["use-string"] = True
+            item_with_dashes = sel_item.get_string()
+            item_with_spaces = item_with_dashes.replace("_", " ")
+            self.settings["string"] = item_with_spaces
+            python = sys.executable
+            os.execl(python, python, *sys.argv)
+        elif response == 'remove':
+            os.system(f"sed -i 's\%s\ \ ' %s/binaries.txt" % (sel_item.get_string(), DATA))
 
 class BTWindow(Gtk.Window):
     def __init__(self, *args, **kwargs):
@@ -39,6 +99,7 @@ class BTWindow(Gtk.Window):
         
         # App menu
         self.menu_button_model = Gio.Menu()
+        self.menu_button_model.append("Show binary texts", 'app.set_binary')
         self.menu_button_model.append(_["about_app"], 'app.about')
         self.menu_button = Gtk.MenuButton.new()
         self.menu_button.set_icon_name(icon_name='open-menu-symbolic')
@@ -85,6 +146,9 @@ class BTWindow(Gtk.Window):
         self.inputEntry.set_title(_["input_entry"])
         self.entryBox.append(self.inputEntry)
         
+        if self.settings["use-string"] == True:
+            self.inputEntry.set_text(self.settings["string"])
+        
         # Output entry
         self.outputEntry = Adw.EntryRow()
         self.outputEntry.set_title(_["output_entry"])
@@ -108,17 +172,34 @@ class BTWindow(Gtk.Window):
         else:
             res = ''.join(format(ord(i), '08b') for i in input_e)
             self.outputEntry.set_text(res)
+            without_spaces = input_e.replace(" ", "_")
+            if not os.path.exists(f"{DATA}/binaries.txt"):
+                with open(f"{DATA}/binaries.txt", "w") as b:
+                    b.write(f'{without_spaces}')
+            else:
+                with open(f"{DATA}/binaries.txt") as rb:
+                    f = rb.read()
+                    if without_spaces in f:
+                        print("It is exists.")
+                    else:
+                        with open(f"{DATA}/binaries.txt", "a") as wb:
+                            wb.write(f'\n{without_spaces}')
             
     def on_close(self, widget, *args):
         (width, height) = self.get_default_size()
         self.settings["window-size"] = (width, height)
         self.settings["is-maximized"] = self.is_maximized()
+        self.settings["use-string"] = False
         
 class BTApp(Adw.Application):
     def __init__(self, **kwargs):
         super().__init__(**kwargs, flags=Gio.ApplicationFlags.FLAGS_NONE)
         self.create_action('about', self.on_about_action, ["F1"])
+        self.create_action('set_binary', self.set_binaries)
         self.connect('activate', self.on_activate)
+        
+    def set_binaries(self, action, param):
+        self.set_d = Dialog_set(self)
         
     def on_about_action(self, action, param):
         dialog = Adw.AboutWindow(transient_for=app.get_active_window())
